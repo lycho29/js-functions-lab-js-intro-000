a5595f1988af50c211d09559f5225ea4dcbf36dd
Lydia Cho <lydiacho@gse.havard.edu>
2020-12-28 03:53:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // write your code below!
-function happyHolidays (); 
+function happyHolidays (String) {
+ return "Happy holidays!"; 
+} 
+
+var name = "John";
+
+function happyHolidaysTo (String) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyCustomHolidayTo (String) {
+  return `Happy ${holiday}, ${name}!`;
+}
